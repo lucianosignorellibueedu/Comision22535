3a26e1b480619553e232c66cf914cf6bf355c3d0
LucianoSignorelli <luciano.signorelli@bue.edu.ar>
2022-09-20 20:32:06 -0300

    Clase 1 Javascript

---
+++ js/javascript-1.js
@@ -0,0 +1,86 @@
+// const IVA =  21;
+// //Aca van los JS
+// console.log('Hola Mundo');
+// //String
+// var nombre = 'Luciano';
+// console.log('Nombre');
+// console.log(nombre);
+// console.log('Nombre con Numero');
+// //Int
+// nombre = 25;
+// console.log(nombre);
+// console.log('nombre con numero de tipo string');
+// //String
+// nombre = "25";
+// console.log(nombre);
+// console.log(2 + 2);
+// //int
+// nombre = 25
+// console.log(nombre + 5)
+// //String
+
+// console.info("La suma de" + nombre + " " + "5", nombre + 5)
+// console.warn(nombre)
+// console.error(nombre)
+// var nombre = true
+// _nombreComision = "Comision22535";
+// console.log(_nombreComision);
+
+// console.clear();
+
+// // IVA= 10.5;
+// // console.log("IVA variable");
+// // console.log(IVA);
+
+
+
+// console.log("IVA constante");
+// console.log(IVA);
+
+// var x = null;
+// console.log("X:")
+// console.log(x);
+// console.clear();
+// var nombre = prompt ("Ingrese su nombre", "")
+// // const saludar = function(nombre){
+// //     console.log("Hola Comision 22535 " +  nombre +"!");
+// // }
+// saludar("Pedro");
+// saludar(32);
+// saludar(true);
+
+// var name = "Juan";
+// console.log(typeof name);
+// console.clear();
+
+// Declarados como objetos
+// const n1 = new Number(4);
+// var n2 = new Number(15.8);
+
+// var a = new Number('123'); // a es igual a 123
+// var b = Number('123'); // b es igual a 123
+// console.log("a: ", a);
+// console.log("b: ", b);
+// console.log(a.isInteger());
+// console.log(b.isInteger());
+
+// var a = 10;
+// var b = "Juan";
+// a++; // a = a + 1;
+// console.log("sin parse");
+// console.log(a + b);
+// console.log("con parse");
+// console.log(parseInt(a) + parseInt(b));
+// console.clear();
+
+var nombre = prompt ("Ingrese su nombre", "")
+
+
+const saludar = function(nombre){
+    document.write("Hola Comision 22535 " +  nombre +"!");
+}
+saludar(nombre);
+
+//const
+//funciones
+//...
